04d73d13d47c9a2ef1f819fb4cb56cda28a5e6da
anandan777 <146792635+anandan777@users.noreply.github.com>
2025-01-25 17:28:04 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,23 @@
-poc
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>takeover poc</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
